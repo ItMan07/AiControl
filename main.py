8656535d19a95b61afe4eeff378c0cc1c86cf7bc
ItMan <itman7144@gmail.com>
2023-12-17 16:25:21 +0500

    update main.py

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ ai = AiHands(
     dist_to_click=50,
     camera_frame_width=1280,
     camera_frame_height=720,
-    flip_code=1,
+    flip_code=None,
     camera_id=0,
 )
-ai.processing()
+
+if __name__ == '__main__':
+    ai.processing()
